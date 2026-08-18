8ef032fa36050225ef22ee6133cd31e02c7ac60c
TGVAD\hzermeno <hzermeno@exsoinf.com>
2026-08-17 18:10:17 -0600

    Pantalla Registros ADC - Completada validación de Excel

---
+++ ICMTools/Application/RegistrosADC/RegistrosADCDetalleExcelDto.vb
@@ -0,0 +1,40 @@
+﻿Public Class RegistrosADCDetalleExcelDto
+
+    <ExcelColumn(0, "ID", Requerido:=True, ValoresIgnorados:={})>
+    Public Property ID As Decimal
+
+    <ExcelColumn(1, "Conteo de Archivos", Requerido:=False, ValoresIgnorados:={})>
+    Public Property ConteoArchivos As Decimal
+
+    <ExcelColumn(2, "CeCo", Requerido:=True, ValoresIgnorados:={})>
+    Public Property Ceco As Decimal
+
+    <ExcelColumn(3, "FechaApproval", Requerido:=True, ValoresIgnorados:={})>
+    Public Property FechaAprobacion As Date
+
+    <ExcelColumn(4, "Accion", Requerido:=False, ValoresIgnorados:={})>
+    Public Property Accion As String
+
+    <ExcelColumn(5, "Ruta", Requerido:=True, ValoresIgnorados:={})>
+    Public Property Ruta As String
+
+    <ExcelColumn(6, "Region", Requerido:=True, ValoresIgnorados:={})>
+    Public Property Region As String
+
+    <ExcelColumn(7, "ComentarioAnalista", Requerido:=False, ValoresIgnorados:={})>
+    Public Property ComentarioAnalista As String
+
+    <ExcelColumn(8, "Nombre Tienda", Requerido:=True, ValoresIgnorados:={})>
+    Public Property NombreTienda As String
+
+    <ExcelColumn(9, "Cargado Por", Requerido:=False, ValoresIgnorados:={})>
+    Public Property CargadoPor As String
+
+    <ExcelColumn(10, "Revisado Expins", Requerido:=False, ValoresIgnorados:={})>
+    Public Property RevisadoExpins As String
+
+    <ExcelColumn(11, "Estatus2", Requerido:=False, ValoresIgnorados:={})>
+    Public Property Estatus As String
+
+
+End Class
---
+++ ICMTools/Application/RegistrosADC/RegistrosADCExcelDto.vb
@@ -0,0 +1,7 @@
+﻿Public Class RegistrosADCExcelDto
+    '<ExcelSheet("PVT", 6)>
+    'Property NA As 
+    <ExcelSheet("BD - Registros ADC", 1)>
+    Property RegistrosADC As List(Of RegistrosADCDetalleExcelDto)
+
+End Class
