fcf480df51b7658313992276856e4bf34761871c
LSpatricio <lpatricio@exsoinf.com>
2026-08-26 15:24:38 -0600

    chore:limpieza código

--- ICMTools/Application/RegistrosADC/RegistrosADCExcelDto.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class RegistrosADCExcelDto
-    '<ExcelSheet("PVT", 6)>
-    'Property NA As 
-    <ExcelSheet("BD - Registros ADC", 1)>
-    Property RegistrosADC As List(Of RegistrosADCDetalleExcelDto)
-
-End Class
