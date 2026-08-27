f3cac4fbb34e8016c1e34947f260793f8de40a98
LSpatricio <lpatricio@exsoinf.com>
2026-08-27 09:40:44 -0600

    feat:agrefar dto referencia excel

---
+++ ICMTools/Application/DTOs/EficienciaExcelDto.vb
@@ -0,0 +1,7 @@
+﻿Public Class EficienciaExcelDto
+    <ExcelColumn("Concat_JOS", Requerido:=True, ValoresIgnorados:={})>
+    Public Property Empleado As String
+    <ExcelColumn("Average of porcentaje_eficiencia_float", ValoresIgnorados:={})>
+    Public Property Promedio As String
+
+End Class
