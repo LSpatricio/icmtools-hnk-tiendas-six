27cdb549ed32e2b5cded442fcb9a10edd915d311
TGVAD\hzermeno <hzermeno@exsoinf.com>
2026-08-28 16:38:56 -0600

    Limpieza de la rama

--- ICMTools/Application/DTOs/EficienciaEfectividad/EficienciaEfectividadExcelDto.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class EficienciaEfectividadExcelDto
-    <ExcelSheet("EFECTIVIDAD", "STG_EFECTIVIDAD", 6)>
-    Property Efectividad As List(Of EfectividadExcelDto)
-    <ExcelSheet("EFICIENCIA", "STG_EFICIENCIA", 6)>
-    Property Eficiencia As List(Of EficienciaExcelDto)
-
-End Class
